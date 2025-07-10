fe0cbbd9a3b567903ad04ce385553124265420d5
Alejandro Bustamante <alejandro.bustamante.dc@gmail.com>
2025-07-10 16:49:29 -0400

    fixed conditional logic in indexer

--- server/internal/service/indexer.go
+++ server/internal/service/indexer.go
@@ -91,10 +91,10 @@ func (x *Indexer) IndexFile(ctx context.Context, info os.FileInfo, path, user st
 		return 0, fmt.Errorf("error checking for the song in the database: %w", err)
 	}
 	trackExists := existsInt == 1
-	if !trackExists {
+	if trackExists {
 		// The return value of -1 in the ID is only to indicate this specific failure
 		// Yes, this value probably should be sent in the error code. This is faster for now
-		return -1, fmt.Errorf("could index the provided track as it already exists in the db: %w", err)
+		return -1, fmt.Errorf("could not index the provided track as it already exists in the db: %w", err)
 	}
 
 	// Check if we already have artist and album for that song
